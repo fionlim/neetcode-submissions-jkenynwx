fa934e4ea52e67f1995c6014b0469e019f405b76
fionlim <98956367+fionlim@users.noreply.github.com>
2026-06-04 19:25:11 +0800

    Add: islands-and-treasure - submission-3

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-3.py
@@ -0,0 +1,34 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        ROWS = len(grid)
+        COLS = len(grid[0])
+
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        def bfs(x, y):
+            # find all tiles that reach 0
+            q = []
+            q.append((x, y, 0))
+            visit = set()
+            
+            while q:
+                r, c, res = q.pop(0)
+                
+                if (r, c) in visit or grid[r][c] == -1:
+                    continue
+                
+                visit.add((r, c))
+                
+                if grid[r][c] != 0 and grid[r][c] != -1:
+                    grid[r][c] = min(grid[r][c], res)
+
+                for dx, dy in directions:
+                    r_new, c_new = r + dx, c + dy
+                    if 0 <= r_new < ROWS and 0 <= c_new < COLS:
+                        q.append((r_new, c_new, res + 1))
+            
+        for x in range(ROWS):
+            for y in range(COLS):
+                if grid[x][y] == 0:
+                    bfs(x, y)
+        
